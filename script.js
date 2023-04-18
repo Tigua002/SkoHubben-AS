425c874a409aac6b72e6af515dbfe4bdbea6d3ef
Tigua002 <tigua002@osloskolen.no>
2023-04-18 13:03:33 +0200

    heil

--- script.js
+++
@@ -1,9 +0,0 @@
-function open_menu() {
-  document.getElementById("menu_content").style.width = "100%"
-  document.getElementById("header_index").style.display = "none"
-
-}
-function close_menu() {
-  document.getElementById("menu_content").style.width = "0%"
-  document.getElementById("header_index").style.display = "flex"
-}
