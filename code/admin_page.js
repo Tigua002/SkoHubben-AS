53e99e33b8a76c7cd96bf973e2f2d3bbeba04df1
Tigua002 <tigua002@osloskolen.no>
2023-04-19 10:08:12 +0200

    e

--- code/admin_page.js
+++ code/admin_page.js
@@ -82,11 +82,10 @@ async function Get_Orders() {
         for (let x = 0; bought_items.length > x; x++) {
             for (let y = 0; y < shoe_types.length; y++) {
                 var shoe = shoe_types[y]
-                console.log(bought_items[x].navn)
-                console.log(data[i].AJ1R)
                 if (bought_items[x].navn == data[i].AJ1R) {
                     console.log("if")
                     bought_items[x].antall += data[i].AJ1R
+                    console.log(data[i].AJ1R)
 
                 }
             }
