9f680e07ee5d8f141b3782f31fa91166657252ad
Tigua002 <tigua002@osloskolen.no>
2023-04-25 16:58:37 +0200

    e

--- code/user_creation.js
+++ code/user_creation.js
@@ -1,6 +1,28 @@
+var usernameEL = document.getElementById("username").value
+var passwordEL = document.getElementById("password").value
+
 function submit_sign_up() {
+
+
+
+}
+
+async function submit_sign_up() {
     var usernameEL = document.getElementById("username").value
     var passwordEL = document.getElementById("password").value
+    const res = await fetch("http://65.108.15.66:22223/get/users",
+        {
+            method: "GET"
+        })
+    var users = await res.json()
+
+    // return users;
+    for (let i = 0; i > users.length; i++){
+        if (users[i].username === usernameEL){
+            alert("Someone else already has that name")
+            return;
+        }
+    }
     if (usernameEL === "") {
         alert("you need to fill inn the username")
         return;
@@ -14,7 +36,6 @@ function submit_sign_up() {
         alert("sorry, no more than 30 characters, shorten the password")
         return;
     }
-    console.log(users + "Ee")
 
     const data = {
         user: usernameEL,
@@ -29,18 +50,7 @@ function submit_sign_up() {
         body: JSON.stringify(data)
         
     })
-
-}
-const users = Get_users()
-async function Get_users() {
-    const res = await fetch("http://65.108.15.66:22223/get/users",
-        {
-            method: "GET"
-        })
-    var users = await res.json()
-    console.log(users)
-    return users;
-
+    
 
     // for (let i = 0; i < data.length; i++) {
     //     bought_items[0].antall += data[i].AJ1R
