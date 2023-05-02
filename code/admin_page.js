71dd78a9b9477ebc073b8f3197614af7d1a4e86b
Tigua002 <tigua002@osloskolen.no>
2023-05-02 11:55:26 +0200

    e

--- code/admin_page.js
+++ code/admin_page.js
@@ -238,6 +238,7 @@ function cause_login() {
     if (sessionStorage.getItem("username")){
         window.location.assign("user_page.html")
     }
+    document.getElementById("login_base").style.display = "flex"
     document.getElementById("packing_pay_page").style.display = "none"
     document.getElementById("empty_packing").style.display = "none"
 
