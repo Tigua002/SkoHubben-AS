c63029d1e7d30b43bbbecda5396a6506e4c5dc9d
Tigua002 <tigua002@osloskolen.no>
2023-05-02 13:46:19 +0200

    e

--- code/index.js
+++ code/index.js
@@ -3,7 +3,6 @@ var x = window.matchMedia("(max-width: 600px)")
 
 /* et globalt variabel som skal skjekke hvilken av knappene ble trykket på */
 if (!x.matches) {
-  console.log("yay")
   var what_input = 0
   var slideIndex = 1;
   showDivs(slideIndex);
@@ -141,6 +140,7 @@ if (!x.matches) {
 
   }
 
+
 } else if (x.matches) {
   for (let spes = 0; spes < 8; spes++) {
     document.getElementById("special").remove()
@@ -150,11 +150,7 @@ if (!x.matches) {
   showDivs(slideIndex);
 
   function plusDivs(n) {
-    if (n === 1) {
-      what_input = 1
-    } else if (n === -1) {
-      what_input = -1
-    }
+    what_input = n
     showDivs(slideIndex += n);
   }
   var x;
@@ -163,44 +159,45 @@ if (!x.matches) {
 
     var x = document.getElementsByClassName("mySlides");
     if (n > x.length) {
-      slideIndex = 1
+      slideIndex = 2
     }
 
 
-    if (n < 1) { slideIndex = x.length }
+    if (n < 2) { slideIndex = x.length }
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     x[slideIndex - 1].style.opacity = "0";
-
     if (what_input === 1) {
       for (let i = 0; i < x.length; i++) {
         document.getElementsByClassName("mySlides")[i].className = "mySlides"
       }
       requestAnimationFrame((time) => {
         requestAnimationFrame((time) => {
-          for (let i = 0; i < x.length; i++) {
-            document.getElementsByClassName("mySlides")[i].className = "mySlides changing"
+          for (let z = 0; z < x.length; z++) {
+            document.getElementsByClassName("mySlides")[z].className = "mySlides changing"
             x[slideIndex - 1].style.opacity = "1";
             x[slideIndex - 1].className = "mySlides animate_in"
+            
           }
         });
       });
     } else {
-      for (let i = 0; i < x.length; i++) {
-        document.getElementsByClassName("mySlides")[i].className = "mySlides"
+      for (let z = 0; z < x.length; z++) {
+        document.getElementsByClassName("mySlides")[z].className = "mySlides"
       }
       requestAnimationFrame((time) => {
         requestAnimationFrame((time) => {
           for (let i = 0; i < x.length; i++) {
             document.getElementsByClassName("mySlides")[i].className = "mySlides changing_out"
-            x[slideIndex - 1].className = "mySlides animate_out"
+            x[slideIndex - 2].className = "mySlides animate_out"
           }
         });
       });
     }
 
     x[slideIndex - 1].style.display = "block";
+    x[slideIndex - 2].style.display = "block";
 
 
   }
@@ -215,11 +212,7 @@ if (!x.matches) {
   showDivs1(slideIndex2);
 
   function plusDivs1(n) {
-    if (n === 1) {
-      what_input1 = 1
-    } else if (n === -1) {
-      what_input1 = -1
-    }
+    what_input1 = n
     showDivs1(slideIndex2 += n);
   }
   var xx;
@@ -227,11 +220,11 @@ if (!x.matches) {
     var ii;
     var xx = document.getElementsByClassName("mySlides1");
     if (n > xx.length) {
-      slideIndex2 = 1
+      slideIndex2 = 2
     }
 
 
-    if (n < 1) { slideIndex2 = xx.length }
+    if (n < 2) { slideIndex2 = xx.length }
     for (ii = 0; ii < xx.length; ii++) {
       xx[ii].style.display = "none";
     }
@@ -257,7 +250,7 @@ if (!x.matches) {
         requestAnimationFrame((time) => {
           for (let ii = 0; ii < xx.length; ii++) {
             document.getElementsByClassName("mySlides1")[ii].className = "mySlides1 changing_out"
-            xx[slideIndex2 - 1].className = "mySlides1 animate_out"
+            xx[slideIndex2 - 2].className = "mySlides1 animate_out"
           }
         });
       });
@@ -265,7 +258,7 @@ if (!x.matches) {
 
 
     xx[slideIndex2 - 1].style.display = "block";
-
+    xx[slideIndex2 - 2].style.display = "block";
   }
 }
 
@@ -288,11 +281,11 @@ function close_menu() {
 
 let touchstartX = 0
 let touchendX = 0
-    
+
 function checkDirection() {
   if (touchendX < touchstartX - 100) {
     plusDivs(1)
-  }else if (touchendX - 100 > touchstartX ) {
+  } else if (touchendX - 100 > touchstartX) {
     plusDivs(-1)
   }
 }
@@ -309,9 +302,9 @@ document.getElementById("slideshow_div1").addEventListener('touchend', e => {
 
 let touchstart = 0
 let touchend = 0
-    
+
 function checkDirection1() {
-  if (touchend < touchstart  - 100) {
+  if (touchend < touchstart - 100) {
     plusDivs1(1)
   } else if (touchend - 100 > touchstart) {
     plusDivs1(-1)
@@ -319,7 +312,7 @@ function checkDirection1() {
 }
 
 document.getElementById("slideshow_div2").addEventListener('touchstart', e => {
-  touchstart = e.changedTouches[0].screenX 
+  touchstart = e.changedTouches[0].screenX
 })
 
 document.getElementById("slideshow_div2").addEventListener('touchend', e => {
