cd01799e7294eeb61e492c7072476f4af3eed616
Tigua002 <tigua002@osloskolen.no>
2023-05-22 10:41:34 +0200

    e

--- code/user_account.js
+++ code/user_account.js
@@ -127,6 +127,7 @@ async function change_username() {
     var users = await res.json()
 
     for (let i = 0; i < users.length; i++) {
+        console.log(users[i].username)
         if (users[i].username.toUppercase()  == orig_user.toUppercase()) {
             alert_tekst.innerHTML = "Someone else already has that name"
             show_alert()
